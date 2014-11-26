4bd1007fbc2829c98e0e5e533eae70e9bdd90066
Hans <hans@lessy.net>
2014-11-26 13:38:26 +0100

    Cache Multi-Key-Aware plus new Version 1.3

--- Common/src/main/java/com/cantaa/util/cache/Cacheable.java
+++ Common/src/main/java/com/cantaa/util/cache/Cacheable.java
@@ -2,12 +2,15 @@ package com.cantaa.util.cache;
 
 /**
  * Classes who's instances can be cached, may implement this interface
+ *
  * @author Hans Lesmeister
  */
 public interface Cacheable {
 
     /**
-     * @return uniquely identifies the cached object (inside the Class-Namespace)
+     * @return Array with keys. Each of them uniquely identifies the cached object (inside the Class-Namespace). In most
+     *         cases you will probably return one value, i.e. the primary key. In addition you can add more keys like an article-
+     *         number
      */
-    Object getCacheIdentifier();
+    Object[] getCacheIdentifiers();
 }
